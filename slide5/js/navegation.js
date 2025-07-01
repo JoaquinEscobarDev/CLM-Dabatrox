23dd67bafe9b788b44e45c595cd6086e34f0ec58
JoaquinEscobarDev <joaquin.escobar.dev@gmail.com>
2025-07-01 15:50:54 -0400

    desasrrollo final aprobado

--- slide5/js/navegation.js
+++
@@ -1,8 +0,0 @@
-function goTo(slide) {
-    console.log('redirige a slide ' + slide)
-    try {
-        com.veeva.clm.gotoSlide(slide + ".zip", "");
-    } catch (error) {
-        window.location.href = '../' + slide + '/' + slide + '.html';
-    }
-}
